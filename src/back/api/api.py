4cb1e61b8da73c876449e97585f594ac2bbe6bfd
Ana Luiza B de Abrantes <100938920+luabrantess@users.noreply.github.com>
2025-04-28 17:04:48 +0000

    criaçao da api

--- src/back/api/api.py
+++ src/back/api/api.py
@@ -1,4 +1,15 @@
-import ipeadatapy as ip
+import requests
+import pandas as pd
 
-todas_series = ip.available_series()
-print(todas_series.head())
+base_url = "http://ipeadata.gov.br/api/odata4/"
+endpoint = "Metadados"
+
+response = requests.get(base_url)
+
+if response.status_code == 200:
+    data = response.json()
+    registros = data.get("value", [])
+    df = pd.DataFrame(registros)
+    print(df.head())
+else:
+    print(f"Erro na requisição: {response.status_code} - {response.text}")
