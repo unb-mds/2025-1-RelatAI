70f5b8db2655b79d9b4ebd0629496cc5ad3abf07
luabrantess <analuizaabr4@gail.com>
2025-05-07 11:21:13 -0300

    inclusao das tabelas do ipea

--- src/back/ipea/financeiro/urls.py
+++ src/back/ipea/financeiro/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('ipca/', views.exibir_ipca, name='exibir_ipca'),
     path('igpm/', views.exibir_igpm, name='exibir_igpm'),
+    path('inpc/', views.exibir_igpm, name='exibir_inpc'),
+    path('cambio/', views.exibir_igpm, name='exibir_cambio'),
+    path('juros/', views.exibir_igpm, name='exibir_taxa_de_juros'),
 
 ]
--- src/back/ipea/financeiro/views.py
+++ src/back/ipea/financeiro/views.py
@@ -18,3 +18,30 @@ def exibir_igpm(request):
         texto = '\n'.join(linhas)
 
     return HttpResponse(texto, content_type='text/plain')
+
+def exibir_inpc(request):
+    caminho_csv = 'data/inpc.csv'
+    with open(caminho_csv, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        linhas = ['\t'.join(row) for row in reader]
+        texto = '\n'.join(linhas)
+
+    return HttpResponse(texto, content_type='text/plain')
+
+def exibir_cambio(request):
+    caminho_csv = 'data/cambio.csv'
+    with open(caminho_csv, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        linhas = ['\t'.join(row) for row in reader]
+        texto = '\n'.join(linhas)
+
+    return HttpResponse(texto, content_type='text/plain')
+
+def exibir_taxa_de_juros(request):
+    caminho_csv = 'data/taxa_de_juros.csv'
+    with open(caminho_csv, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        linhas = ['\t'.join(row) for row in reader]
+        texto = '\n'.join(linhas)
+
+    return HttpResponse(texto, content_type='text/plain')
