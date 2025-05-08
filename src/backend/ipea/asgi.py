b633f11c1799fd014e331fc5368c786f12903e62
Pedro Rocha Ferreira Lima <pedro.lima@tc.df.gov.br>
2025-05-08 16:21:58 -0300

    Criando e comitando em teste

--- src/backend/ipea/asgi.py
+++ src/backend/ipea/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ipea project.
+ASGI config for relatorioipea project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ipea.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'relatorioipea.settings')
 
 application = get_asgi_application()
--- src/backend/ipea/wsgi.py
+++ src/backend/ipea/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ipea project.
+WSGI config for relatorioipea project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ipea.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'relatorioipea.settings')
 
 application = get_wsgi_application()
